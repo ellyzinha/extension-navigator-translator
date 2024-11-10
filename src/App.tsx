91a19680f34d76d6e4a1f6f79c8003d149cc032b
ellyzinha <driellymaciel@hotmail.com>
2024-11-09 22:35:05 -0300

    adding the selection event, to only show the icon when there's a selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import './App.css';
 import Modal from './components/Modal';
+import TextTest from './components/TextTest';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      const selection = window.getSelection();
+      console.log(selection);
+      // se a seleção não estiver colapsada, chama o componente Modal
+      if (selection && !selection.isCollapsed) {
+        setIsModalOpen(true);
+      } else {
+        setIsModalOpen(false);
+      }
+    };
+
+    window.addEventListener('mouseup', handleMouseUp);
+
+    // Limpa o evento quando o componente é desmontado
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, []);
+
   return (
-    <Modal/>
+    <>
+      <TextTest />
+      {isModalOpen && <Modal />}
+    </>
   );
 }
 
